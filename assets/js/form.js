c3605d31f38a066766a4f3b88f4c7b8080fbf7e6
Nancy Ramon <nramon513@gmail.com>
2024-06-28 22:47:53 -0500

    added local storage

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,12 @@
-console.log("What's on your mind?")
+const userName = document.getElementById('username');
+const title = document.getElementById('title');
+const content = document.getElementById('content');
+const submitButton = document.getElementById('submit');
+
+function saveContent() {
+    const userName = {
+    userName: userName.value,
+      title: title.value,
+      content: content.value.trim(),
+    };
+    localStorage.setItem('userName', JSON.stringify(userName));
