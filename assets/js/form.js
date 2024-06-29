b7929004ea1bdb12891e613f5e9915db071fdd1f
Nancy Ramon <nramon513@gmail.com>
2024-06-29 00:38:41 -0500

    corrected local storage errors

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,12 +1,26 @@
-const userName = document.getElementById('username');
-const title = document.getElementById('title');
-const content = document.getElementById('content');
+
 const submitButton = document.getElementById('submit');
 
-function saveContent() {
-    const userName = {
-    userName: userName.value,
-      title: title.value,
-      content: content.value.trim(),
-    };
-    localStorage.setItem('userName', JSON.stringify(userName));
+function saveContent(event) {
+    event.preventDefault();
+    const userName = document.getElementById('username');
+    const title = document.getElementById('title');
+    const content = document.getElementById('content');
+    let userData = JSON.parse(localStorage.getItem("userData"))
+    const userDetails= {
+        userName: userName.value,
+          title: title.value,
+          content: content.value.trim(),
+        };
+    if (userData) {
+        userData.push(userDetails)
+    } else {
+        userData = []
+        userData.push(userDetails)
+    }
+    
+  
+    localStorage.setItem('userData', JSON.stringify(userData));
+}
+submitButton.addEventListener('click' , saveContent)
+    
