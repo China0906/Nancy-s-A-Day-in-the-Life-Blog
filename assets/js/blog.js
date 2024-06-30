571ac116c3f21ddb8a7eb53139b8c0503045360a
Nancy Ramon <nramon513@gmail.com>
2024-06-29 21:57:58 -0500

    added toggle light effect

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,12 +1,6 @@
-const themeButtonEl = $('#theme-btn');
 
-let isDark = true;
-themeButtonEl.on('click', function () {
-    if (isDark) {
-      $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
-      isDark = !isDark;
-    } else {
-      $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
-      isDark = !isDark;
-    }
-  });
+function myFunction(onClick) {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+
